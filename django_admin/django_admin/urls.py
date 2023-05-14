f41b0dcd7f36b9caeef20f2e72cf538263c74646
joybs1999 <joybrata.s@supersourcing.com>
2023-05-14 12:01:21 +0530

    model migrated

--- django_admin/django_admin/urls.py
+++ django_admin/django_admin/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    # path('auth/',include('auth.urls')),
+    path('auth/',include('authentication.urls')),
     # path('test_socket/',include('test_socket.urls')),
 
 
